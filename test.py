48067def081e829378b366c50b983df853a6da5d
AmSheep <chenyanweione@163.com>
2019-12-05 16:50:00 +0800

    修改test.py 增加一个循环练习

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 #-*- encoding:utf-8 -*—
-print("Hello World")
+while True:
+    try:
+        x = int(input('Please enter a number:'))
+        break
+    except ValueError:
+        print('Not a valid number.Try again...')
+print('Your number is:', x)
